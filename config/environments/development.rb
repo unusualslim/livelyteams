3b06050e6a95611e2d97f551b18672b08b54993c
unusualslim <lance@perrybrothersoil.com>
2022-07-22 15:58:41 -0400

    styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,20 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options ={:host => ENV['HOSTNAME'], :protocol => 'https'}
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'livelyteams.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
