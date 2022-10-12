192e8f1a778c935a8b5c9340231c3afbc3a0ea36
unusualslim <lance@perrybrothersoil.com>
2022-10-12 16:34:33 -0400

    turbo:load

--- app/javascript/src/geeks_table_search.js
+++ app/javascript/src/geeks_table_search.js
@@ -1,4 +1,4 @@
-  $(document).ready(function() {
+  $(document).on('page:load', functions($) {
     $(".table_search").on("keyup", function() {
       var value = $(this).val().toLowerCase();
       $(".searchable tr").filter(function() {
