876da1a31e2d78fda6fde0228354349cda1955e4
Lance Perry <unusualslim@gmail.com>
2022-10-13 19:12:48 +0000

    first_name last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
   has_many :comment
   has_many :cases, through: :case_users
 
+  def full_name                                                                                                                                                                                     
+    ([first_name, last_name] - ['']).compact.join(' ')                         
+  end
+
 #  has_many :team_members
 #  has_many :teams, through: :team_members
 #  has_many :roles, through: :team_members
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_203442) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_175436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -276,6 +276,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_203442) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
