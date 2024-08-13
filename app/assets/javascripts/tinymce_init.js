d54130e086f0bb93eb4c1ce2740e35eaedcbe33a
John Smarr <john.smarr97@gmail.com>
2024-08-13 12:20:28 -0400

    tinymce change to quill

--- app/assets/javascripts/tinymce_init.js
+++ app/assets/javascripts/tinymce_init.js
@@ -1,19 +1,33 @@
-document.addEventListener('DOMContentLoaded', function() {
-  if (typeof tinymce !== 'undefined') {
-    tinymce.remove();
-    tinymce.init({
-        selector: 'textarea',
-        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
-        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-        tinycomments_mode: 'embedded',
-        tinycomments_author: 'Author name',
-        mergetags_list: [
-          { value: 'First.Name', title: 'First Name' },
-          { value: 'Email', title: 'Email' },
-        ],
-        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
-      });
-    } else {
-      console.error('TinyMCE is not defined');
+document.addEventListener("DOMContentLoaded", () => {
+  const hiddenField = document.getElementById('quill-content');
+  console.log("Hidden Field:", hiddenField); // Debugging line
+
+  if (hiddenField) {
+    const noteContent = hiddenField.value;
+    const htmlMarkup = noteContent;
+
+    const quill = new Quill('#editor', {
+      theme: 'snow',
+      modules: {
+        toolbar: [
+          [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
+          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+          ['bold', 'italic', 'underline'],
+          [{ 'align': [] }],
+          ['link', 'image']
+        ]
+      }
+    });
+
+    if (noteContent) {
+      quill.clipboard.dangerouslyPasteHTML(htmlMarkup);
     }
+
+    quill.on('text-change', () => {
+      console.log("Quill content updated:", quill.root.innerHTML);
+      hiddenField.value = quill.root.innerHTML;
+    });
+  } else {
+    console.error("Element with ID 'quill-content' not found.");
+  }
 });
