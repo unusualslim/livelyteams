9343ad3e0acfe8f31b3ed642ede3293c90cb18e9
John Smarr <john.smarr97@gmail.com>
2024-11-25 14:57:45 -0500

    updated skip_recaptcha

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,5 +81,5 @@ config.action_mailer.smtp_settings = {
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.skip_recaptcha = true
+  config.skip_recaptcha = false
 end
