3f9f1aef2d3d732ca4b3ce038f24e32e4491b76e
John Smarr <john.smarr97@gmail.com>
2026-02-10 11:55:09 -0500

    update team member model

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,5 +1,15 @@
 class TeamMember < ApplicationRecord
-#  belongs_to :user
-#  belongs_to :team
-#  belongs_to :role
+  belongs_to :user
+  belongs_to :team
+  belongs_to :role
+
+  def move_to!(team_name:, role_name:)
+    new_team = Team.find_by!(name: team_name)
+    new_role = Role.find_by!(role: role_name)
+
+    update!(
+      team: new_team,
+      role: new_role
+    )
+  end
 end
