7e48a71f676b02746be6b07777acfbe7f810ec0b
John Smarr <john.smarr97@gmail.com>
2026-02-20 16:58:29 -0500

    fix case mailer to work with additional emails

--- app/mailers/case_mailer.rb
+++ app/mailers/case_mailer.rb
@@ -12,147 +12,137 @@ class CaseMailer < ApplicationMailer
     )
   end
 
+  private
+
+  def email_enabled?(user)
+    return false if user.nil?
+    user.notification_method == 'email' || user.notification_method == 'both'
+  end
+
+  def add_recipients!(recipient_array, user, preference_flag)
+    return if user.nil?
+    return unless email_enabled?(user)
+
+    # preference_flag is a symbol like :new_case_created, :updated_case, etc.
+    return unless user.respond_to?(preference_flag) && user.public_send(preference_flag)
+
+    # notification_recipients returns [primary_email + extra active emails]
+    recipient_array.concat(user.notification_recipients)
+  end
+
+  public
+
   def new_case_email(new_case)
     @case = new_case
-    recipient = []
+    recipients = []
 
     @case.case_users.each do |cu|
-      if cu.user.new_case_created && (cu.user.notification_method == 'email' || cu.user.notification_method == 'both')
-        recipient.push(cu.user.email)
-      end
+      add_recipients!(recipients, cu.user, :new_case_created)
     end
 
-    if @case.requested_by.new_case_created && (@case.requested_by.notification_method == 'email' || @case.requested_by.notification_method == 'both')
-      recipient.push(@case.requested_by.email)
-    end
+    add_recipients!(recipients, @case.requested_by, :new_case_created)
+    add_recipients!(recipients, @case.assigned_to, :new_case_created)
 
-    if @case.assigned_to.new_case_created && (@case.assigned_to.notification_method == 'email' || @case.assigned_to.notification_method == 'both')
-      recipient.push(@case.assigned_to.email)
-    end
+    recipients = recipients.compact.uniq
 
     mail(
-      to: recipient.uniq,
+      to: recipients,
       subject: "[#{@case.severity.severity}] Case No. #{@case.id} #{@case.subject}"
-    ) unless recipient.empty?
+    ) unless recipients.empty?
   end
 
   def update_case_email(updated_case)
     @case = updated_case
-    recipient = []
+    recipients = []
 
     @case.case_users.each do |cu|
-      if cu.user.updated_case && (cu.user.notification_method == 'email' || cu.user.notification_method == 'both')
-        recipient.push(cu.user.email)
-      end
+      add_recipients!(recipients, cu.user, :updated_case)
     end
 
-    if @case.requested_by.updated_case && (@case.requested_by.notification_method == 'email' || @case.requested_by.notification_method == 'both')
-      recipient.push(@case.requested_by.email)
-    end
+    add_recipients!(recipients, @case.requested_by, :updated_case)
+    add_recipients!(recipients, @case.assigned_to, :updated_case)
 
-    if @case.assigned_to.updated_case && (@case.assigned_to.notification_method == 'email' || @case.assigned_to.notification_method == 'both')
-      recipient.push(@case.assigned_to.email)
-    end
+    recipients = recipients.compact.uniq
 
     mail(
-      to: recipient.uniq,
+      to: recipients,
       subject: "Case No. #{@case.id} has been updated"
-    ) unless recipient.empty?
+    ) unless recipients.empty?
   end
 
   def new_comment_case_email(updated_case)
     @case = updated_case
-    recipient = []
+    recipients = []
 
     @case.case_users.each do |cu|
-      if cu.user.new_comment && (cu.user.notification_method == 'email' || cu.user.notification_method == 'both')
-        recipient.push(cu.user.email)
-      end
+      add_recipients!(recipients, cu.user, :new_comment)
     end
 
-    if @case.requested_by.new_comment && (@case.requested_by.notification_method == 'email' || @case.requested_by.notification_method == 'both')
-      recipient.push(@case.requested_by.email)
-    end
+    add_recipients!(recipients, @case.requested_by, :new_comment)
+    add_recipients!(recipients, @case.assigned_to, :new_comment)
 
-    if @case.assigned_to.new_comment && (@case.assigned_to.notification_method == 'email' || @case.assigned_to.notification_method == 'both')
-      recipient.push(@case.assigned_to.email)
-    end
+    recipients = recipients.compact.uniq
 
     mail(
-      to: recipient.uniq,
+      to: recipients,
       subject: "Case No. #{@case.id} has a new comment"
-    ) unless recipient.empty?
+    ) unless recipients.empty?
   end
 
   def inspectable_case_email(updated_case)
     @case = updated_case
-    recipient = []
+    recipients = []
 
     @case.case_users.each do |cu|
-      if cu.user.inspectable_case && (cu.user.notification_method == 'email' || cu.user.notification_method == 'both')
-        recipient.push(cu.user.email)
-      end
+      add_recipients!(recipients, cu.user, :inspectable_case)
     end
 
-    if @case.requested_by.inspectable_case && (@case.requested_by.notification_method == 'email' || @case.requested_by.notification_method == 'both')
-      recipient.push(@case.requested_by.email)
-    end
+    add_recipients!(recipients, @case.requested_by, :inspectable_case)
+    add_recipients!(recipients, @case.assigned_to, :inspectable_case)
 
-    if @case.assigned_to.inspectable_case && (@case.assigned_to.notification_method == 'email' || @case.assigned_to.notification_method == 'both')
-      recipient.push(@case.assigned_to.email)
-    end
+    recipients = recipients.compact.uniq
 
     mail(
-      to: recipient.uniq,
+      to: recipients,
       subject: "Case No. #{@case.id} is ready for inspection"
-    ) unless recipient.empty?
+    ) unless recipients.empty?
   end
 
   def billable_case_email(updated_case)
     @case = updated_case
-    recipient = []
+    recipients = []
 
     @case.case_users.each do |cu|
-      if cu.user.billable_case && (cu.user.notification_method == 'email' || cu.user.notification_method == 'both')
-        recipient.push(cu.user.email)
-      end
+      add_recipients!(recipients, cu.user, :billable_case)
     end
 
-    if @case.requested_by.billable_case && (@case.requested_by.notification_method == 'email' || @case.requested_by.notification_method == 'both')
-      recipient.push(@case.requested_by.email)
-    end
+    add_recipients!(recipients, @case.requested_by, :billable_case)
+    add_recipients!(recipients, @case.assigned_to, :billable_case)
 
-    if @case.assigned_to.billable_case && (@case.assigned_to.notification_method == 'email' || @case.assigned_to.notification_method == 'both')
-      recipient.push(@case.assigned_to.email)
-    end
+    recipients = recipients.compact.uniq
 
     mail(
-      to: recipient.uniq,
+      to: recipients,
       subject: "Case No. #{@case.id} is complete and ready for billing"
-    ) unless recipient.empty?
+    ) unless recipients.empty?
   end
 
   def closed_case_email(updated_case)
     @case = updated_case
-    recipient = []
+    recipients = []
 
     @case.case_users.each do |cu|
-      if cu.user.closed_case && (cu.user.notification_method == 'email' || cu.user.notification_method == 'both')
-        recipient.push(cu.user.email)
-      end
+      add_recipients!(recipients, cu.user, :closed_case)
     end
 
-    if @case.requested_by.closed_case && (@case.requested_by.notification_method == 'email' || @case.requested_by.notification_method == 'both')
-      recipient.push(@case.requested_by.email)
-    end
+    add_recipients!(recipients, @case.requested_by, :closed_case)
+    add_recipients!(recipients, @case.assigned_to, :closed_case)
 
-    if @case.assigned_to.closed_case && (@case.assigned_to.notification_method == 'email' || @case.assigned_to.notification_method == 'both')
-      recipient.push(@case.assigned_to.email)
-    end
+    recipients = recipients.compact.uniq
 
     mail(
-      to: recipient.uniq,
+      to: recipients,
       subject: "Case No. #{@case.id} is now closed."
-    ) unless recipient.empty?
+    ) unless recipients.empty?
   end
 end
