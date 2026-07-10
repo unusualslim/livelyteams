f370c5bdb8744fc6d34f90f5536e32cc74ce5f6f
John Smarr <john.smarr97@gmail.com>
2026-07-10 09:51:32 -0400

    Add attachment upload and delete to case show page
    Adds an inline file upload form directly on the case show page so users
    can attach files without navigating to edit. Also adds delete buttons on
    each attachment card and authentication guard to AttachmentsController.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,4 +1,6 @@
 class AttachmentsController < ApplicationController
+  before_action :authenticate_user!
+
   def purge
     attachment = ActiveStorage::Attachment.find(params[:id])
     attachment.purge
